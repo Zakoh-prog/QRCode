1c1328aaa3f402b660dc5409949bd117eaed7c5b
Zakoh-prog <60792405+Zakoh-prog@users.noreply.github.com>
2020-05-26 14:07:24 +0200

    rendu propre sans foritures

--- qrcodepdf.js
+++ qrcodepdf.js
@@ -1,16 +1,17 @@
 var element = $("#aff"); // global variable
 var getCanvas; // global variable
-
-    $("#btn-Preview-Image").on('click', function () {
-         html2canvas(element, {
-         onrendered: function (canvas) {
-                $("#previewImage").append(canvas);
-                getCanvas = canvas;
-             }
-         });
+    
+    //  
+    jQuery(document).ready(function($){
+        html2canvas(element, {
+            onrendered: function (canvas) {
+                   getCanvas = canvas;
+                }
+            });
     });
 
     $("#btn-Convert-Html2Image").on('click', function () {
+        
         var imgageData = getCanvas.toDataURL("image/png");
         // Now browser starts downloading it instead of just showing it
         var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
